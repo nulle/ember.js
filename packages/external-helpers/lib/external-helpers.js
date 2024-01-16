148bc448f3b288777a7921e8955411933b0652c9
Inga Brūnava <nulle@users.noreply.github.com>
2024-01-16 12:06:43 +0100

    Add extends export to ember-babel

--- packages/external-helpers/lib/external-helpers.js
+++ packages/external-helpers/lib/external-helpers.js
@@ -193,3 +193,23 @@ export function createForOfIteratorHelperLoose(o) {
   i = o[Symbol.iterator]();
   return i.next.bind(i);
 }
+
+function _extends() {
+  _extends = Object.assign ? Object.assign.bind() : function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+export {
+  _extends as extends,
+};
